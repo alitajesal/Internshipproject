6cf7c9dee756f8938fc9bbad5c4a8de01ce9c3e5
alitajesal <42314445+alitajesal@users.noreply.github.com>
2018-08-12 15:07:53 +0530

    commit

---
+++ sketch_aug12a.ino
@@ -0,0 +1,40 @@
+const int s0=9;
+const int s1=10;
+const int s2=11;
+const int s3=12;
+const int en=8;
+const int sig=7;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(s0,OUTPUT);
+  pinMode(s1,OUTPUT);
+  pinMode(s2,OUTPUT);
+  pinMode(s3,OUTPUT);
+
+  pinMode(en,OUTPUT);
+  pinMode(sig,OUTPUT);
+//  pinMode(s0,OUTPUT);
+//  pinMode(s0,OUTPUT);
+  digitalWrite(s0, LOW);
+digitalWrite(s1, HIGH);
+digitalWrite(s2, LOW);
+digitalWrite(s3, LOW);
+
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(en, LOW);
+
+digitalWrite(sig, HIGH);
+delay(5000);
+digitalWrite(en, HIGH);
+delay(5000);
+
+
+
+}
